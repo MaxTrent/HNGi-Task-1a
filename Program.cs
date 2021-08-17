e7e3eaf29746fff95d9cfc9a4e7c716e16f05e9e
Samuel Adelowo <83916293+MaxTrent@users.noreply.github.com>
2021-08-17 23:17:38 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace name
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Hello User, input your name");
+            name = Console.ReadLine();
+            Console.WriteLine("Your name is {0}", name);
+            Console.ReadKey();
+        }
+    }
+}
